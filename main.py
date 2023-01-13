30d575625095faaa46e1d8decb73b690d66f0d59
hugopm <hugo.pm@protonmail.com>
2023-01-13 11:56:20 +0100

    faster goto admin

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
 import time
 
 
-driver = webdriver.Firefox()
+opt = webdriver.FirefoxOptions()
+opt.headless = True
+driver = webdriver.Firefox(options=opt)
 
 driver.get("https://concours.algorea.org")
 
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 30)
+driver.implicitly_wait(30)
 
 # Store the ID of the original window
 original_window = driver.current_window_handle
@@ -24,7 +28,6 @@ original_window = driver.current_window_handle
 assert len(driver.window_handles) == 1
 
 # Click the link which opens in a new window
-driver.implicitly_wait(10)
 span_connect = driver.find_element(By.CSS_SELECTOR, '[ng-i18next="login_connect"]')
 assert span_connect.text == "Se connecter"
 span_connect.click()
@@ -56,12 +59,15 @@ login_form.submit()
 wait.until(EC.number_of_windows_to_be(1))
 
 driver.switch_to.window(original_window)
-driver.find_element(By.CLASS_NAME, "menu-item-login").click()
+# driver.find_element(By.CLASS_NAME, "menu-item-login").click()
 
-driver.find_element(By.CSS_SELECTOR, '[ng-i18next="[title];groupRequests_title_my_groups"]').click()
+# driver.find_element(By.CSS_SELECTOR, '[ng-i18next="[title];groupRequests_title_my_groups"]').click()
 
-links = driver.find_element(By.CSS_SELECTOR, '[ng-show="myGroupAdmin.children.length"]').find_elements(By.TAG_NAME, 'a')
-next(filter(lambda l: l.text == "Sélection olympiades 2023 phase 2", links)).click()
+# links = driver.find_element(By.CSS_SELECTOR, '[ng-show="myGroupAdmin.children.length"]').find_elements(By.TAG_NAME, 'a')
+# next(filter(lambda l: l.text == "Sélection olympiades 2023 phase 2", links)).click()
+
+wait.until(EC.presence_of_element_located([By.CLASS_NAME, "menu-item-login"]))
+driver.get("https://concours.algorea.org/groupAdmin/70330347151610819/")
 
 driver.find_element(By.CSS_SELECTOR, '[ng-i18next="[title];progress"]').click()
 driver.find_element(By.CSS_SELECTOR, """[uib-btn-radio="'collective'"]""").click()
@@ -70,9 +76,12 @@ div_parcours_select = driver.find_element(By.CLASS_NAME, "parcours-select").find
 assert len(div_parcours_select) == 2
 Select(div_parcours_select[0]).select_by_visible_text("Sélection olympiades 2023")
 
-wait.until(EC.presence_of_element_located([By.CSS_SELECTOR, '[uib-tooltip="Épreuve de pré-selection 1"]']))
+wait.until(EC.presence_of_element_located([By.CSS_SELECTOR, '[uib-tooltip="Épreuve de sélection 1"]']))
 Select(div_parcours_select[1]).select_by_visible_text("Épreuve de sélection 1")
 
+wait.until(EC.presence_of_element_located([By.CSS_SELECTOR, '[uib-tooltip="Épreuve de sélection 1"]']))
+to_screen = driver.find_element(By.CSS_SELECTOR, """[ng-if="formValues.progressionType == 'collective'"]""").find_element(By.CLASS_NAME, "table-wrapper")
+to_screen.screenshot("test.png")
 
 try:
     time.sleep(20)
